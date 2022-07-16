cf15ff2e7a72bc4a6acd573c7e312e7981a2a9db
Nikita Martsenkovsky <martsenkovskynikita@icloud.com>
2022-07-17 00:31:07 +0700

    Добавил blinding-и для текстовых полей, окна редактирования контакта.

--- ContactApp/ViewModel/ContactWindowViewModel.cs
+++ ContactApp/ViewModel/ContactWindowViewModel.cs
@@ -7,27 +7,60 @@ namespace ContactApp.ViewModel
 {
     class ContactWindowViewModel : ViewModelBase
     {
-        private string _title;
-
-        public string Title
-        {
-            get => _title;
-            set => Set(ref _title, value);
-        }
-
-
         /// <summary>
         /// Текстовое поле фамилии.
         /// </summary>
-        private string _contactWondowSurname;
+        private string _surname;
 
+        /// <summary>
+        /// Текстовое поле имени.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Текстовое поле отчества.
+        /// </summary>
+        private string _patronymic;
+
+        /// <summary>
+        /// Текстовое поле отчества.
+        /// </summary>
+        private string _phone;
+ 
         /// <summary>
         /// Задает и возвращает текстовое поле фамилии.
         /// </summary>
-        public string ContactWindowSurname
+        public string Surname
         {
-            get => _contactWondowSurname;
-            set => Set(ref _contactWondowSurname, value);
+            get => _surname;
+            set => Set(ref _surname, value);
+        }
+
+        /// <summary>
+        /// Задает и возвращает текстовое поле имени.
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        /// <summary>
+        /// Задает и возвращает текстовое поле отчества.
+        /// </summary>
+        public string Patronymic
+        {
+            get => _patronymic;
+            set => Set(ref _patronymic, value);
+        }
+
+        /// <summary>
+        /// Задает и возвращает текстовое поле номера телефона.
+        /// </summary>
+        public string Phone
+        {
+            get => _phone;
+            set => Set(ref _phone, value);
         }
     }
 }
