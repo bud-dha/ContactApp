db7ad13adc921ffeb2de542e3288b406f74e34ad
Nikita Martsenkovsky <martsenkovskynikita@icloud.com>
2022-08-08 23:06:06 +0700

    Привязал данные выбранного контакта к правой панели.

--- ContactApp/ViewModel/MainWindowViewModel.cs
+++ ContactApp/ViewModel/MainWindowViewModel.cs
@@ -19,33 +19,13 @@ namespace ContactApp.ViewModel
         /// <summary>
         /// Объект класса Contact.
         /// </summary>
-        private Contact _contact;
+        private Contact _selectedcontact;
 
         /// <summary>
         /// Текстовое поле id.
         /// </summary>
         private int _id;
-
-        /// <summary>
-        /// Текстовое поле имени.
-        /// </summary>
-        private string _surname;
-
-        /// <summary>
-        /// Текстовое поле фамилии.
-        /// </summary>
-        private string _name;
-
-        /// <summary>
-        /// Текстовое поле отчества.
-        /// </summary>
-        private string _patronymic;
-
-        /// <summary>
-        /// Текстовое поле номера телефона.
-        /// </summary>
-        private string _phone;
-
+        
         /// <summary>
         /// Задает и возвращает объект класса Project.
         /// </summary>
@@ -58,13 +38,13 @@ namespace ContactApp.ViewModel
         /// <summary>
         /// Задает и возвращает объект класса Contact.
         /// </summary>
-        public Contact Contact
+        public Contact SelectedContact
         {
-            get => _contact;
+            get => _selectedcontact;
             set 
             {
-                _contact = value;
-                OnPropertyChanged("Contact");
+                _selectedcontact = value;
+                OnPropertyChanged("SelectedContact");
             }
         }
 
@@ -80,43 +60,7 @@ namespace ContactApp.ViewModel
         {
             get => _id;
             set => Set(ref _id, value);
-        }
-
-        /// <summary>
-        /// Задает и возвращает текстовое поле фамилии.
-        /// </summary>
-        public string Surname
-        {
-            get => _surname;
-            set => Set(ref _surname, value);
-        }
-
-        /// <summary>
-        /// Задает и возвращает текстовое поле имени.
-        /// </summary>
-        public string Name
-        {
-            get => _name;
-            set => Set(ref _name, value);
-        }
-
-        /// <summary>
-        /// Задает и возвращает текстовое поле отчества.
-        /// </summary>
-        public string Patronymic
-        {
-            get => _patronymic;
-            set => Set(ref _patronymic, value);
-        }
-
-        /// <summary>
-        /// Задает и возвращает текстовое поле номера телефона.
-        /// </summary>
-        public string Phone
-        {
-            get => _phone;
-            set => Set(ref _phone, value);
-        }
+        }       
 
         #endregion
 
@@ -145,17 +89,16 @@ namespace ContactApp.ViewModel
 
         public MainWindowViewModel()
         {
-            CloseAplicationCommand = new LambdaCommand(OnCloseAplicationCommandExecuted, CanCloseAplicationCommandExecut);
-
             _project = ProjectSerializer.LoadFromFile();
+
+            CloseAplicationCommand = new LambdaCommand(OnCloseAplicationCommandExecuted, CanCloseAplicationCommandExecut);     
 
             Contacts = new ObservableCollection<Contact>();
 
             foreach (var items in _project.Contacts)
             {
                 Contacts.Add(items);
-            }
-
+            }            
         }
 
 
