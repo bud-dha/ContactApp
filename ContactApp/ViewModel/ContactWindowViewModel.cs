024108dbb9eeaf232d9da270e55a0aa0e500c650
martsenkovskynikita <martsenkovskyn@gmail.com>
2022-08-26 18:32:09 +0700

    Добавил команду закрытия дочернего окна

--- ContactApp/ViewModel/ContactWindowViewModel.cs
+++ ContactApp/ViewModel/ContactWindowViewModel.cs
@@ -15,10 +15,25 @@ namespace ContactApp.ViewModel
 
         #region Команды
 
+        #region Команда принятия изменений
+
+        public ICommand AceptChangesCommand { get; }
+
+        private void OnAceptChangesCommandExecuted(object p)
+        {
+            Window win = p as Window;
+            win.Close();
+        }
+
+        private bool CanAceptChangesCommandExecuted(object p) => true;
+
+        #endregion
+
         #endregion
 
         public ContactWindowViewModel()
         {
+            AceptChangesCommand = new LambdaCommand(OnAceptChangesCommandExecuted, CanAceptChangesCommandExecuted);
         }
     }
 }
